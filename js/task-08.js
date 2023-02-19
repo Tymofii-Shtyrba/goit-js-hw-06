78b535e712a07cb34f10eb42d47664069ce9618d
Tymofii-Shtyrba <timofeyshtyrba@gmail.com>
2023-02-19 04:17:18 +0200

    Done the eighth task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const formRef = document.querySelector('.login-form');
+const fieldsRefs = formRef.querySelectorAll('input');
 
+formRef.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const isCompleted = iaFormCompleted();
+
+  if (isCompleted) {
+    const theCompletedForm = {};
+    theCompletedForm.email = formRef.firstElementChild.firstElementChild.value;
+    theCompletedForm.password = formRef.firstElementChild.nextElementSibling.firstElementChild.value;
+    console.log(theCompletedForm);
+    formRef.reset();
+  } else {
+    alert('All form fields must be completed!');  
+  }
+  
+  
+});
+
+function iaFormCompleted() {
+  return fieldsRefs[0].value && fieldsRefs[1].value;
+}
