aadb74a808957f8dec831f6cc07a6476c641f181
Tymofii-Shtyrba <timofeyshtyrba@gmail.com>
2023-02-19 20:34:27 +0200

    Added changes

--- js/task-01.js
+++ js/task-01.js
@@ -1,26 +1,11 @@
 
 const categoriesRefs = document.querySelectorAll('.item');
 
-console.log(categoriesRefs);
-
 console.log('Number of categories:', categoriesRefs.length);
 
 for (const categoryRef of categoriesRefs) {
   const header = categoryRef.firstElementChild.textContent
-  console.log(header);
   console.log('Category:', header);
   const categoryElementsRef = categoryRef.lastElementChild.children
   console.log('Elements:', categoryElementsRef.length);
 }
-
-// const categoryCounter = document.querySelectorAll(".item");
-
-// console.log("Number of categories: ", categoryCounter.length);
-
-// categoryCounter.forEach(function (category) {
-//   console.log(category);
-//   const categoryHeader = category.querySelector("h2");
-//   const elementsCounter = category.querySelectorAll("li");
-//   console.log("\nCategory: ", categoryHeader.textContent);
-//   console.log("Elements: ", elementsCounter.length);
-// });
